d6cddf13cad30653e96d2f593653c0782ac3d67d
Gaurav rajbhar <pugbap384@gmail.com>
2022-06-29 09:43:28 +0530

    initial styleing

--- src/Components/Square.jsx
+++ src/Components/Square.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 //object destructing
 function square({ value }) {
-  return <button type="button">{value}</button>;
+  return (
+    <button type="button" className="square">
+      {value}
+    </button>
+  );
 }
 
 export default square;
